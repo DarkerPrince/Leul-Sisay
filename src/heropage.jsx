0b9281e8229354d312083d4c1ba5b1fa4e2de02f
luel_work <princemagiv@gmail.com>
2023-03-08 13:23:20 +0300

    hero image is fixed

--- src/heropage.jsx
+++ src/heropage.jsx
@@ -8,7 +8,9 @@ function HeroPage() {
   return (
     
       <div className="snap-start flex relative justify-center md:h-[100vh] bg-gradient-to-b from-slate-800 to-slate-900 items-start bg-contain bg-center bg-no-repeat object-contain">
-        <div className="flex flex-col md:flex-row bg-center gap-6 md:gap-32 w-full md:mx-12  bg-no-repeat bg-contain items-center justify-center">
+        <div className="flex flex-col md:flex-row bg-center gap-6 md:gap-32 w-full md:mx-12   bg-no-repeat bg-contain items-center justify-center">
+        
+         
           <div
             className="flex flex-col justify-center md:items-start items-center text-left "
           >
@@ -46,7 +48,7 @@ function HeroPage() {
 
           {/* <!-- image placer for the face of mine --> */}
          
-           <Heroimage/>
+          <Heroimage />
           
        
         </div>
