9ae661d887de0bfe3af7394a70aee589637c7019
luel_work <princemagiv@gmail.com>
2023-03-11 20:47:50 +0300

    -- Final

--- src/footerPage.jsx
+++ src/footerPage.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function FooterComponent() {
   return (
-   
-<footer className="bottom-0 left-0 z-20 w-full p-4  border-t  md:flex md:items-center md:justify-between md:p-6 bg-slate-800 border-slate-600">
+    <footer className="bottom-0 left-0 z-50 w-full p-4  border-t  md:flex md:items-center md:justify-between md:p-6 bg-slate-800 border-slate-600">
     <span className="text-sm  sm:text-center text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
         <li>
-            <a href="/" className="mr-4 hover:underline md:mr-6 ">Blogposts</a>
+        <Link to={"blog"}><p className="mr-4 hover:underline md:mr-6 ">Blogposts</p></Link>
         </li>
         <li>
-            <a href="/" className="mr-4 hover:underline md:mr-6">Gallery</a>
+            <a href="#gallery" className="mr-4 hover:underline md:mr-6">Gallery</a>
         </li>
         <li>
-            <a href="/" className="mr-4 hover:underline md:mr-6">Categories</a>
+            <a href="#category" className="mr-4 hover:underline md:mr-6">Category</a>
         </li>
         <li>
-            <a href="/" className="hover:underline">Profession</a>
+         <a href="#skill" className="mr-4 hover:underline md:mr-6">Skills</a>
+        </li>
+        <li>
+        <a href="#profession" className="hover:underline">Profession</a>
         </li>
     </ul>
 </footer>
-
   )
 }
 
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -2,10 +2,11 @@ import HeroPage from "./heropage";
 import SkillsPage from "./skillspage";
 import CertificationPage from "./certificationpage";
 import CarrerSummary from "./careersummary";
+import FooterComponent from "./footerPage";
 
 import Categories from "./categories";
 
-
+import { Link } from "react-router-dom";
 import WorkGallery from "./gallerypage";
 
 
@@ -15,11 +16,18 @@ function Homepage() {
   return (
     <div className="font-leuleFont md:snap-proximity snap-y">
       <HeroPage />
+      <div id="category">
       <Categories />
+      </div>
+      <div id="skill">
       <SkillsPage />
+      </div>
+      <div id="gallery">
       <WorkGallery/>
+      </div>
       <CertificationPage />
       <CarrerSummary />
+    
     </div>
   );
 }
